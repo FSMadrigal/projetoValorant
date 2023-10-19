ea8aeb5b5d310004786e3a930521d8bfe07eb5c9
FSMadrigal <msmittyh@gmail.com>
2023-10-19 02:41:05 -0300

    Adaptar Tela para Mobile
    Adapta as diferentes telas do projeto para mobile.

--- projeto_valorant_front/src/app/pages/individual-weapon-page/individual-weapon-page.component.ts
+++ projeto_valorant_front/src/app/pages/individual-weapon-page/individual-weapon-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import KeenSlider, { KeenSliderInstance } from 'keen-slider';
 import { WeaponsService } from 'src/app/api/weapons.service';
 
 @Component({
@@ -7,8 +8,10 @@ import { WeaponsService } from 'src/app/api/weapons.service';
   templateUrl: './individual-weapon-page.component.html',
   styleUrls: ['./individual-weapon-page.component.scss']
 })
-export class IndividualWeaponPageComponent implements OnInit {
+export class IndividualWeaponPageComponent implements OnInit, AfterViewInit {
+  @ViewChild('sliderRef') sliderRef!: ElementRef<HTMLElement>;
   individual_weapon: any;
+  slider!: KeenSliderInstance;
 
   constructor(
     protected weaponService: WeaponsService,
@@ -19,5 +22,17 @@ export class IndividualWeaponPageComponent implements OnInit {
     const weaponName = this.activatedRoute.params.subscribe(param => {
       this.individual_weapon = this.weaponService.getWeaponByName(param['nome_da_arma']);
     });
+  }
+
+  ngAfterViewInit() {
+    const sliderConfig: any = {
+      loop: true,
+      mode: "free-snap",
+      slides: {
+        perView: 2,
+        spacing: 30,
+      }
+    }
+    this.slider = new KeenSlider(this.sliderRef.nativeElement, sliderConfig);
   }
 }
